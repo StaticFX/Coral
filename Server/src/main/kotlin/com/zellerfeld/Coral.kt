0ac439bbf0c4917809d95e5f0851639cb8d1cca9
Devin Fritz <devinfritz@gmx.de>
2023-06-07 14:23:57 +0200

    Restructured project

--- Server/src/main/kotlin/com/zellerfeld/Coral.kt
+++ Server/src/main/kotlin/com/zellerfeld/Coral.kt
@@ -6,7 +6,11 @@ package com.zellerfeld
  * Its manages the bot by wrapping around the JDA Api. To make executions and programing simpler Coral provides a lot of functions and builders to minimize code.
  *
  * Coral is build from different modules
+ *  - API containing the main developer API for extensions
+ *  - HTTP containing the http api to control coral
  *
+ * Coral itself only provides a connection to the discord bot and very limited functionality.
+ * To fully use coral extensions need to be added, therefore the extensions api can be used.
  *
  */
 object Coral {
@@ -16,9 +20,8 @@ object Coral {
     }
 
     private fun boot() {
-
-
-
+        println("www")
     }
+
 
 }
